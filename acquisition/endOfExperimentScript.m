ae5451472b6e2578a8cb61e650db7756740a1bd6
Michael Krumin <mkrumin@gmail.com>
2019-05-02 15:30:29 +0100

    Added endOfExperimentScript for a convenient way to bin full data for the whole experiment and copy things to the server

---
+++ acquisition/endOfExperimentScript.m
@@ -0,0 +1,79 @@
+animalName = 'PC037';
+expDate = '2019-04-16';
+
+question = sprintf('Process data of %s for %s?', animalName, expDate);
+button = questdlg(question,'fUSi post exp script','OK','Cancel','Cancel');
+if isequal(button, 'Cancel')
+    % abort
+    return;
+else
+    % otherwise continue
+end
+
+%% get list of experiments for the date
+
+p = dat.paths;
+localFolderName = fullfile(p.localRepository, animalName, expDate);
+subfolders = dir(localFolderName);
+subfolders = subfolders([subfolders.isdir]);
+subfolders = subfolders(~ismember({subfolders.name}, {'.', '..'}));
+
+ExpRefs = cell(0);
+ExpRefsStr = '';
+for iSub = 1:length(subfolders)
+    % detect all the '_fus.mat' files and extract the ExpRef
+    files = dir(fullfile(localFolderName, subfolders(iSub).name, '*_fus.mat'));
+    for iFile = 1:length(files)
+        ExpRefs{end+1} = strrep(files(iFile).name, '_fus.mat', '');
+        ExpRefsStr = cat(2, ExpRefsStr, [ExpRefs{end}, char(10)]);
+    end
+end
+
+question = sprintf('Will be processing the following experiments:\n%s', ExpRefsStr);
+button = questdlg(question,'fUSi post exp script','OK','Cancel','Cancel');
+if isequal(button, 'Cancel')
+    % abort
+    return;
+else
+    % otherwise continue
+end
+
+
+%% create a local backup copy of the data
+tic;
+localBackupFolderName = fullfile(p.localRepository, animalName, [expDate, '_bkp']);
+fprintf('Creating local backup copy ..');
+[st, msg, msgID] = copyfile(localFolderName, localBackupFolderName);
+if st
+    fprintf('. success!\n')
+    fprintf('Folder %s created\n', localBackupFolderName);
+    toc;
+else
+    fprintf('. fail!\n')
+    fprintf('Error message: %s\n', msg);
+    fprintf('Error message ID: %s\n', msgID);
+    fprintf('Canceling the whole thing ... \n');
+    return;
+end
+%% bin full beam-formed data
+tStart = tic;
+for iExp = 1:length(ExpRefs)
+    binBF(ExpRefs{iExp});
+end
+fprintf('\nTotal time taken to bin BF: %1.0f seconds\n', toc(tStart));
+
+%% copy files to server
+serverFolderName = fullfile(p.mainRepository, animalName, expDate);
+fprintf('Copying the data to the server (%s) ..', serverFolderName);
+[st, msg, msgID] = copyfile(localFolderName, serverFolderName);
+if st
+    fprintf('. success!\n')
+    fprintf('Data successfully copied to %s\n', serverFolderName);
+    toc;
+else
+    fprintf('. fail!\n')
+    fprintf('There was some issue copying the data to the server:\n');
+    fprintf('Error message: %s\n', msg);
+    fprintf('Error message ID: %s\n', msgID);
+    return;
+end
